ed1185af24f99c2d48e0210d12282cc7813e0282
Alan O'Donnell <alan.m.odonnell@gmail.com>
2014-06-11 12:41:26 -0400

    Tweak

--- repeatem.js
+++ repeatem.js
@@ -1,11 +1,12 @@
 var interval = null;
 $('#content-wrapper').on('click', '.section-track a.play-ctrl', function() {
   clearInterval(interval);
-  var trackId = $(this).attr('id').substring("play_ctrl_".length);
+  var $playButton = $(this);
+  var trackId = $playButton.attr('id').substring("play_ctrl_".length);
   var $track = $("#section-track-" + trackId);
   interval = setInterval(function() {
     if (!$track.hasClass('haarp-active')) {
-      $("a.play-ctrl", $track)[0].click();
+      $playButton[0].click();
     }
   }, 1000);
 });
